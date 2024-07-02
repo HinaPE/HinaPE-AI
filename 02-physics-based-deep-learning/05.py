7e868e78b08087d4fb703312562658d2a0067d97
Xayah <ycxiang14sep@gmail.com>
2024-07-03 04:16:39 +0900

    update

--- 02-physics-based-deep-learning/05.py
+++ 02-physics-based-deep-learning/05.py
@@ -1,29 +1,43 @@
 from phi.torch.flow import *
 
-velocity = StaggeredGrid(0, x=8, y=8, bounds=Box(x=100, y=100))  # or CenteredGrid(...)
-smoke = CenteredGrid(0, ZERO_GRADIENT, x=8, y=8, bounds=Box(x=100, y=100))
-INFLOW = 0.2 * resample(Sphere(x=50, y=9.5, radius=5), to=smoke, soft=True)
+velocity = StaggeredGrid((0, 0, 0), 0, x=50, y=50, z=50, bounds=Box(x=1, y=1, z=1))  # or CenteredGrid(...)
+smoke = CenteredGrid(0, ZERO_GRADIENT, x=50, y=50, z=50, bounds=Box(x=1, y=1, z=1))
+INFLOW = 0.5 * resample(Sphere(x=0.5, y=0.5, z=0.5, radius=0.1), to=smoke, soft=True)
 pressure = None
 
 
-@jit_compile  # Only for PyTorch, TensorFlow and Jax
+# @jit_compile  # Only for PyTorch, TensorFlow and Jax
 def step(v, s, p, dt=1.):
     s = advect.mac_cormack(s, v, dt) + INFLOW
-    buoyancy = resample(s * (0, 0.1), to=v)
+    buoyancy = resample(s * (0, 0, 0.1), to=v)
     v = advect.semi_lagrangian(v, v, dt) + buoyancy * dt
-    v, p = fluid.make_incompressible(v, (), Solve(x0=p))
+    v, p = fluid.make_incompressible(v, (), Solve('auto', 1e-5, x0=p))
     return v, s, p
 
 
-velocity, smoke, pressure = step(velocity, smoke, pressure)
-velocity, smoke, pressure = step(velocity, smoke, pressure)
-velocity, smoke, pressure = step(velocity, smoke, pressure)
-velocity, smoke, pressure = step(velocity, smoke, pressure)
-velocity, smoke, pressure = step(velocity, smoke, pressure)
+# velocity, smoke, pressure = step(velocity, smoke, pressure)
+#
+# d_n = smoke.data.native('x,y,z')
+# v_xn = velocity.vector['x'].data.native('x,y,z')
+# v_yn = velocity.vector['y'].data.native('x,y,z')
+# v_zn = velocity.vector['z'].data.native('x,y,z')
+#
+# DEBUG = True
+#
+# if DEBUG:
+#     print(smoke.data.shape)
+#     print(type(smoke.data))
+#     print(type(d_n))
+#     print(d_n.size())
+#
+#     print(velocity.data.shape)
+#     print(type(velocity.data))
+#     print(type(v_xn))
+#     print(type(v_yn))
+#     print(type(v_zn))
+#     print(v_xn.size())
+#     print(v_yn.size())
+#     print(v_zn.size())
 
-print(type(smoke.data))
-print(smoke.data.shape)
-# print(smoke.data.numpy('y,x'))
-
-print(smoke.data.dimension)
-# print(f"{smoke.data:full:shape}")
+for _ in view(smoke, velocity, 'pressure', play=False, namespace=globals()).range(warmup=1):
+    velocity, smoke, pressure = step(velocity, smoke, pressure)
