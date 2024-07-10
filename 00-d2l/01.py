04a3c1ad0fbc37d9c579b95a5c969b1c464c7103
Xayah-Hina <ycxiang14sep@gmail.com>
2024-07-10 15:44:11 +0900

    update

--- 00-d2l/01.py
+++ 00-d2l/01.py
@@ -1,36 +1,29 @@
 import torch
-import torchvision
-
-from torch.utils import data
-from torchvision import transforms
-from IPython import display
+from torch import nn
 from d2l import torch as d2l
-
-
-def get_dataloader_workers():
-    return 4
-
-
-def load_data_fashion_mnist(batch_size, resize=None):
-    trans = [transforms.ToTensor()]
-    if resize:
-        trans.insert(0, transforms.Resize(resize))
-    trans = transforms.Compose(trans)
-    mnist_train = torchvision.datasets.FashionMNIST(
-        root="../data", train=True, transform=trans, download=True)
-    mnist_test = torchvision.datasets.FashionMNIST(
-        root="../data", train=False, transform=trans, download=True)
-    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
-                            num_workers=get_dataloader_workers()),
-            data.DataLoader(mnist_test, batch_size, shuffle=False,
-                            num_workers=get_dataloader_workers()))
-
 
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-num_inputs = 784
-num_outputs = 10
-W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
-b = torch.zeros(num_outputs, requires_grad=True)
+# PyTorch不会隐式地调整输入的形状。因此，
+# 我们在线性层前定义了展平层(flatten)，来调整网络输入的形状
+
+# 我们首先定义一个模型变量net，它是一个Sequential类的实例
+# Sequential类将多个层串联在一起。当给定输入数据时，Sequential实例将数据传入到第一层，然后将第一层的输出作为第二层的输入，以此类推。
+# 在下面的例子中，我们的模型只包含一个层，因此实际上不需要Sequential。但是由于以后几乎所有的模型都是多层的，在这里使用Sequential会让你熟悉“标准的流水线”。
+net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+
+net.apply(init_weights);
+
+loss = nn.CrossEntropyLoss(reduction='none')
+trainer = torch.optim.SGD(net.parameters(), lr=0.1)
+
+num_epochs = 10
+d2l.train_ch6(net, train_iter, test_iter, loss, num_epochs, trainer)
 
