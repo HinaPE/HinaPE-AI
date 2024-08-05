22a5d66cdf0af348ebca525861b2819c29d8bb12
Xayah <ycxiang14sep@gmail.com>
2024-08-06 00:56:44 +0900

    update

--- 00-d2l/01.py
+++ 00-d2l/01.py
@@ -2,12 +2,12 @@ import torch
 from d2l import torch as d2l
 from torch import nn
 
-n_train, n_test, num_inputs, batch_size = 20, 100, 200, 5
-true_w, true_b = torch.ones((num_inputs, 1)) * 0.01, 0.05
-train_data = d2l.synthetic_data(true_w, true_b, n_train)
-train_iter = d2l.load_array(train_data, batch_size)
-test_data = d2l.synthetic_data(true_w, true_b, n_test)
-test_iter = d2l.load_array(test_data, batch_size, is_train=False)
+# n_train, n_test, num_inputs, batch_size = 20, 100, 200, 5
+# true_w, true_b = torch.ones((num_inputs, 1)) * 0.01, 0.05
+# train_data = d2l.synthetic_data(true_w, true_b, n_train)
+# train_iter = d2l.load_array(train_data, batch_size)
+# test_data = d2l.synthetic_data(true_w, true_b, n_test)
+# test_iter = d2l.load_array(test_data, batch_size, is_train=False)
 
 
 # def init_params():
@@ -42,26 +42,31 @@ test_iter = d2l.load_array(test_data, batch_size, is_train=False)
 # d2l.plt.show()
 
 
-def train_concise(wd):
-    net = nn.Sequential(nn.Linear(num_inputs, 1))
-    for param in net.parameters():
-        param.data.normal_()
-    loss = nn.MSELoss(reduction='none')
-    num_epochs, lr = 100, 0.003
-    # 偏置参数没有衰减
-    trainer = torch.optim.SGD([{"params": net[0].weight, 'weight_decay': wd}, {"params": net[0].bias}], lr=lr)
-    animator = d2l.Animator(xlabel='epochs', ylabel='loss', yscale='log', xlim=[5, num_epochs],
-                            legend=['train', 'test'])
-    for epoch in range(num_epochs):
-        for X, y in train_iter:
-            trainer.zero_grad()
-            l = loss(net(X), y)
-            l.mean().backward()
-            trainer.step()
-        if (epoch + 1) % 5 == 0:
-            animator.add(epoch + 1, (d2l.evaluate_loss(net, train_iter, loss), d2l.evaluate_loss(net, test_iter, loss)))
-    print('w的L2范数：', net[0].weight.norm().item())
+# def train_concise(wd):
+#     net = nn.Sequential(nn.Linear(num_inputs, 1))
+#     for param in net.parameters():
+#         param.data.normal_()
+#     loss = nn.MSELoss(reduction='none')
+#     num_epochs, lr = 100, 0.003
+#     # 偏置参数没有衰减
+#     trainer = torch.optim.SGD([{"params": net[0].weight, 'weight_decay': wd}, {"params": net[0].bias}], lr=lr)
+#     animator = d2l.Animator(xlabel='epochs', ylabel='loss', yscale='log', xlim=[5, num_epochs],
+#                             legend=['train', 'test'])
+#     for epoch in range(num_epochs):
+#         for X, y in train_iter:
+#             trainer.zero_grad()
+#             l = loss(net(X), y)
+#             l.mean().backward()
+#             trainer.step()
+#         if (epoch + 1) % 5 == 0:
+#             animator.add(epoch + 1, (d2l.evaluate_loss(net, train_iter, loss), d2l.evaluate_loss(net, test_iter, loss)))
+#     print('w的L2范数：', net[0].weight.norm24().item())
+#
+#
+# train_concise(3)
+# d2l.plt.show()
 
-
-train_concise(3)
-d2l.plt.show()
+# X = torch.ones((6, 8))
+# print(X.reshape((1, 1, 6, 8)))
+conv2d = nn.Conv2d(1,1, kernel_size=(1, 2), bias=False)
+print(conv2d.weight.data)
